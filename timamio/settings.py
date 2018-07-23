0579d8bdec2b67311bef5db5f94261dba55a2fae
Timam <me@timam.io>
2018-07-24 02:36:43 +0600

    whitenose storage configured

--- timamio/settings.py
+++ timamio/settings.py
@@ -47,6 +47,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',                               # Simplified static file serving.
 ]
 
 ROOT_URLCONF = 'timamio.urls'
@@ -126,3 +127,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
