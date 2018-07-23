42f5e18bcd0c83356a84a90d54f4b3d6aefd463b
Timam <me@timam.io>
2018-07-24 02:28:10 +0600

    static working

--- timamio/settings.py
+++ timamio/settings.py
@@ -54,9 +54,8 @@ ROOT_URLCONF = 'timamio.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        # 'DIRS': [],
         'DIRS': [
-            BASE_DIR + '/templates/',
+            os.path.join(BASE_DIR, 'templates'),
         ],
         'APP_DIRS': True,
         'OPTIONS': {
@@ -121,4 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
