afcb807ecd0b01a25e01c553cc84c2a9ad276f62
Timam <me@timam.io>
2018-07-24 01:11:25 +0600

    aboutme url set to root url

--- timamio/urls.py
+++ timamio/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('about/', include('aboutme.urls')),
+    path('', include('aboutme.urls')),
     path('admin/', admin.site.urls),
 ]
